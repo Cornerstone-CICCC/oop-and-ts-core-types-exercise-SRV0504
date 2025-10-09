c6bc088119660de63a0f8023892ae3d965888047
Samuel Rios <srios050402@gmail.com>
2025-10-08 18:59:44 -0700

    Ex-2 complete

--- src/ex02.ts
+++ src/ex02.ts
@@ -1,12 +1,20 @@
 // Exercise: Create an enum `DaysOfWeek` with values for each day of the week.
 // Then, write a function `isWeekend` that accepts a value from the enum and returns `true` if it's Saturday or Sunday, and `false` otherwise.
 
-function isWeekend(day) {
-
-}
-
-console.log(isWeekend(DaysOfWeek.Saturday));
-console.log(isWeekend(DaysOfWeek.Wednesday));
-// Expected output:
-// true
-// false
+enum DaysOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+  
+  function isWeekend(day: DaysOfWeek): boolean {
+    return day === DaysOfWeek.Saturday || day === DaysOfWeek.Sunday;
+  }
+  
+  console.log(isWeekend(DaysOfWeek.Saturday));  // true
+  console.log(isWeekend(DaysOfWeek.Wednesday)); // false
+  
